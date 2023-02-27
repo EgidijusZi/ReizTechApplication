528bcaaa29ccd2aa4eafcc89d7c6738d847bf669
EgidijusZi <zvicevicius.egidijus@gmail.com>
2023-02-27 17:53:20 +0200

    Added method for depth

--- Branches/Branches/Branch.cs
+++ Branches/Branches/Branch.cs
@@ -3,12 +3,18 @@
     public class Branch<T>
     {
         public T BranchData { get; set; }
-        public List<Branch<T>> Branches { get; set; }
+        public List<Branch<T>> branches { get; set; }
 
         public Branch(T branchData)
         {
             BranchData = branchData;
-            Branches = new List<Branch<T>>();
+            branches = new List<Branch<T>>();
+        }
+
+        public List<Branch<T>> Branches
+        {
+            get { return branches; }
+            set { branches = value; }
         }
 
         public void AddBranch(Branch<T> branch)
@@ -19,6 +25,18 @@
         public void RemoveBranch(Branch<T> branch)
         {
             Branches.Remove(branch);
+        }
+
+        public static int Depth(Branch<T> root, int depth)
+        {
+            int result = depth + 1;
+
+            foreach (Branch<T> branch in root.Branches)
+            {
+                result = Math.Max(result, Depth(branch, depth + 1));
+            }
+
+            return result;
         }
     }
 }
